0be54fe464c1c20ff64b30f09e474861350d8801
Gleb Pashchenko <pashenkogleb@gmail.com>
2022-01-22 00:27:14 +0300

    added pandas util functions

---
+++ gutil/pandas.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import tqdm
+
+def apply(s, func):
+    '''
+    apply with progress tracker
+    '''
+    res =[]
+    for x in tqdm.tqdm(s):
+        res.append(func(x))
+    return pd.Series(res,index= s.index)

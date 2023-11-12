447a711cba589cab079a6d15ede98264e49e9d3d
Gleb Pashchenko <pashenkogleb@gmail.com>
2023-11-12 17:07:15 +0000

    upd

--- gutil/seasonal.py
+++ gutil/seasonal.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.dates as mdates
 
-def plot_seasonal(series, apply_differencer = False):
+def plot_seasonal(series, apply_differencer = False, resample = None):
     '''
     takes a series as an input
+    can provide resample = "1w" to aggregate by week if data is not available for every day
     '''
     if apply_differencer:
         series = Differencer(na_handling="keep_na").fit_transform(series)
@@ -17,7 +18,11 @@ def plot_seasonal(series, apply_differencer = False):
         plt.plot(uniform_s,label= year, alpha=0.1)
         uniform.append(uniform_s)
     uniform = pd.concat(uniform,axis=1)
-    uniform.mean(axis=1).plot()
+    m = uniform.mean(axis=1)
+    if resample is not None:
+        m = m.resample(resample).mean()
+                    
+    m.plot()
     plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
     

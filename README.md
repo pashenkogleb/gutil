3f90f915775aaca8130f0aa6f47ced2b8f1e61a6
Gleb Pashchenko <pashenkogleb@gmail.com>
2025-06-15 17:31:33 +0100

    imports

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 to use locally : pip install -e .
 
+install directly: pip install git+https://github.com/pashenkogleb/gutil
+
 to reload after code changes: gutil.reload()
--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+
+
 setup(
    name='gutil',
    version='1.0',
@@ -7,5 +9,6 @@ setup(
    author='Gleb Pashchenko',
    author_email='pashenkogleb@gmail.com',
    packages=find_packages(),  #same as name
-   install_requires=["lightgbm>=4.0.0", "scikit-learn>=0.24.0", "pandas>=1.2.0", "numpy>=1.19.0"], #external packages as dependencies
+   install_requires=["lightgbm>=4.0.0", "scikit-learn>=0.24.0", "pandas>=1.2.0", "numpy>=1.19.0", "tqdm>=4.62.0", "selenium", "sympy", "IPython", "requests" , "beautifulsoup4", "matplotlib", "sktime", "numba"], #external packages as dependencies
+
 )
